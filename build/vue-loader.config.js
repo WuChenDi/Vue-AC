541ce7fb8167f312a61b57d2d6d7fa91e63a23ac
WuChenDi <996194720@qq.com>
2018-10-11 16:29:09 +0800

    Add vue-loader.config file function annotation

--- build/vue-loader.config.js
+++ build/vue-loader.config.js
@@ -1,6 +1,12 @@
 module.exports = (isDev) => {
   return {
+    // 去除template后面空格问题
     preserveWhitepace: true,
-    extractCSS: true
+    // 打包css样式文件到单独文件中
+    extractCSS: !isDev,
+    // 实现cssModules功能
+    cssModules: {},
+    // 热重载功能
+    hotReload: true
   }
 }
