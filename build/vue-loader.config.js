4ec0f6175a7c6431f655e5a3d3c9e639b6f03c71
WuChenDi <996194720@qq.com>
2018-10-12 09:40:06 +0800

    实现cssModules功能

--- build/vue-loader.config.js
+++ build/vue-loader.config.js
@@ -1,3 +1,5 @@
+// const docsLoader = require.resolve('./doc-loader')
+
 module.exports = (isDev) => {
   return {
     // 去除template后面空格问题
@@ -5,8 +7,13 @@ module.exports = (isDev) => {
     // 打包css样式文件到单独文件中
     extractCSS: !isDev,
     // 实现cssModules功能
-    cssModules: {},
+    cssModules: {
+      localIdentName: '[path]-[name]-[hash:base64:5]',
+      camelCase: true
+    },
     // 热重载功能(根据环境变量生成)
     // hotReload: true
+    // preLoader: {},
+    // postLoader: {}
   }
 }
